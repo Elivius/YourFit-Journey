f43ef720f1a5e9983de4524b91851b7f02e9a203
Elivius <elivius.dev@elivius.de>
2025-05-21 21:02:29 +0800

    V1.12 - Removed Session

--- user-site/backend/process_login.php
+++ user-site/backend/process_login.php
@@ -34,8 +34,7 @@ if ($stmt = mysqli_prepare($connection, $sql)) {
         if (password_verify($password, $user['password'])) {
             // Secure session handling
             session_regenerate_id(true);
-
-            $_SESSION['logged_in'] = true;
+            
             $_SESSION['user_id']   = $user['user_id'];
 
             mysqli_stmt_close($stmt);
