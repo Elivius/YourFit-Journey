aa56c015d475c269545c937751902ab394369bd2
Elivius <elivius.dev@elivius.de>
2025-06-16 21:33:31 +0800

    V1.47 - Change workout card colour

--- user-site/frontend/assets/js/workouts.js
+++ user-site/frontend/assets/js/workouts.js
@@ -20,31 +20,31 @@ document.addEventListener('DOMContentLoaded', function() {
     });
     
     // Add to My Workouts Button
-    const addToMyWorkoutsBtn = document.querySelector('.card-actions .btn-primary');
+    // const addToMyWorkoutsBtn = document.querySelector('.card-actions .btn-primary');
     
-    if (addToMyWorkoutsBtn) {
-        addToMyWorkoutsBtn.addEventListener('click', function() {
-            // Here you would typically add the workout to the user's saved workouts
-            // For demo purposes, we'll just show an alert
-            alert('Workout added to My Workouts!');
+    // if (addToMyWorkoutsBtn) {
+    //     addToMyWorkoutsBtn.addEventListener('click', function() {
+    //         // Here you would typically add the workout to the user's saved workouts
+    //         // For demo purposes, we'll just show an alert
+    //         alert('Workout added to My Workouts!');
             
-            // Change button text to indicate it's been added
-            this.innerHTML = '<i class="fas fa-check"></i> Added to My Workouts';
-            this.classList.add('btn-success');
-            this.classList.remove('btn-primary');
+    //         // Change button text to indicate it's been added
+    //         this.innerHTML = '<i class="fas fa-check"></i> Added to My Workouts';
+    //         this.classList.add('btn-success');
+    //         this.classList.remove('btn-primary');
             
-            // Disable the button to prevent multiple clicks
-            this.disabled = true;
+    //         // Disable the button to prevent multiple clicks
+    //         this.disabled = true;
             
-            // After a delay, reset the button
-            setTimeout(() => {
-                this.innerHTML = '<i class="fas fa-plus"></i> Add to My Workouts';
-                this.classList.remove('btn-success');
-                this.classList.add('btn-primary');
-                this.disabled = false;
-            }, 3000);
-        });
-    }
+    //         // After a delay, reset the button
+    //         setTimeout(() => {
+    //             this.innerHTML = '<i class="fas fa-plus"></i> Add to My Workouts';
+    //             this.classList.remove('btn-success');
+    //             this.classList.add('btn-primary');
+    //             this.disabled = false;
+    //         }, 3000);
+    //     });
+    // }
     
     // Print Workout Button
     const printWorkoutBtn = document.querySelector('.card-actions .btn-outline-primary');
